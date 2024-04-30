c1deea09ff61e724c9bda2260a7a64f42f4362c9
Kyle Krull <kdkrull@gmail.com>
2024-04-30 00:32:01 -0500

    Make ssh-agent plugin less annoying

--- zsh/.zshrc.d/ssh-agent.zsh
+++ zsh/.zshrc.d/ssh-agent.zsh
@@ -1,2 +1,5 @@
 
+# Wait until I'm good and ready, to unlock my key
+# https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/ssh-agent#lazy
+zstyle :omz:plugins:ssh-agent lazy yes
 plugins+=(ssh-agent)
