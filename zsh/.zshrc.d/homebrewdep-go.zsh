ce7f392dc6920977ad31fe79821f5f244b8cdeae
Kyle Krull <kdkrull@gmail.com>
2024-06-24 09:48:11 -0500

    Restore support for /usr/local/go

--- zsh/.zshrc.d/homebrewdep-go.zsh
+++ zsh/.zshrc.d/homebrewdep-go.zsh
@@ -1,13 +1,14 @@
 
 golang_home_debian="/usr/local/go"
-
-if type go >/dev/null
-then
-  # Homebrew
-  local gopath="$(go env GOPATH)"
-  path=("$gopath/bin" $path)
-elif [[ -d "$golang_home_debian/bin" ]]
+if [[ -d "$golang_home_debian/bin" ]]
 then
   # https://go.dev/doc/install, v1.22.4
   path=("$golang_home_debian/bin" $path)
+fi
+
+if type go >/dev/null
+then
+  # Debian, Homebrew
+  local gopath="$(go env GOPATH)"
+  path=("$gopath/bin" $path)
 fi
