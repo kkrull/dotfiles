b86780770453d4d7d5e9cb06640c669da67b9778
Kyle Krull <kdkrull@gmail.com>
2025-01-23 16:47:02 -0600

    Add homebrew path for nvim

--- zsh/.zshrc.d/vim.zsh
+++ zsh/.zshrc.d/vim.zsh
@@ -1,2 +1,19 @@
 
-export EDITOR=/usr/bin/vim
+if [[ -e /opt/homebrew/bin/nvim ]]
+then
+  export EDITOR=/opt/homebrew/bin/nvim
+elif [[ -e /usr/bin/nvim ]]
+then
+  export EDITOR=/usr/bin/nvim
+elif [[ -e /usr/bin/vim ]]
+then
+  export EDITOR=/usr/bin/vim
+fi
+
+if [[ -v EDITOR ]]
+then
+  if type git >/dev/null
+  then
+    export GIT_EDITOR="$EDITOR"
+  fi
+fi
