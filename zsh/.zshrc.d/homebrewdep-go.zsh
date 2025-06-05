1b30e762ffcc2bc2a60ccc57e242a9d374c81d61
Kyle Krull <kdkrull@gmail.com>
2025-06-05 01:22:45 -0500

    Put those paths back where they were

--- zsh/.zshrc.d/homebrewdep-go.zsh
+++ zsh/.zshrc.d/homebrewdep-go.zsh
@@ -3,12 +3,14 @@ golang_home_debian="/usr/local/go"
 if [[ -d "$golang_home_debian/bin" ]]
 then
   # https://go.dev/doc/install, v1.22.4
-  path=("$golang_home_debian/bin" "$path")
+  # shellcheck disable=SC2206 # not compatible with zsh array-style path
+  path=("$golang_home_debian/bin" $path)
 fi
 
 if type go >/dev/null
 then
   # Debian, Homebrew
   # shellcheck disable=SC2128
-  path=("$(go env GOPATH)/bin" "$path")
+  # shellcheck disable=SC2206 # not compatible with zsh array-style path
+  path=("$(go env GOPATH)/bin" $path)
 fi
--- zsh/.zshrc.d/python3.zsh
+++ zsh/.zshrc.d/python3.zsh
@@ -2,5 +2,6 @@
 homebrew_path="/opt/homebrew/opt/python@3"
 if [[ -d "$homebrew_path" ]]
 then
-  path=("$homebrew_path/libexec/bin" "$path")
+  # shellcheck disable=SC2206 # not compatible with zsh array-style path
+  path=("$homebrew_path/libexec/bin" $path)
 fi
