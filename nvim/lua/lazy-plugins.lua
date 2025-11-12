0ec786cf1b1b7aee1344d65e4a4f23f81a8fcf0a
Kyle Krull <kdkrull@gmail.com>
2025-11-12 12:03:17 -0600

    Cleanup

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -1,56 +1,5 @@
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
--- NOTE: Plugins can also be added by using a table,
--- with the first argument being the link and the following
--- keys can be used to configure plugin behavior/loading/etc.
---
--- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
---
--- Alternatively, use `config = function() ... end` for full control over the configuration.
--- If you prefer to call `setup` explicitly, use:
---    {
---        'lewis6991/gitsigns.nvim',
---        config = function()
---            require('gitsigns').setup({
---                -- Your gitsigns configuration here
---            })
---        end,
---    }
---
--- NOTE: Plugins can also be configured to run Lua code when they are loaded.
---
--- This is often very useful to both group configuration, as well as handle
--- lazy loading plugins that don't need to be loaded immediately at startup.
---
--- For example, in the following configuration, we use:
---  event = 'VimEnter'
---
--- which loads which-key before all the UI elements are loaded. Events can be
--- normal autocommands events (`:help autocmd-events`).
---
--- Then, because we use the `opts` key (recommended), the configuration runs
--- after the plugin has been loaded as `require(MODULE).setup(opts)`.
---
--- NOTE: Plugins can specify dependencies.
---
--- The dependencies are proper plugin specifications as well - anything
--- you do for a plugin at the top level, you can do for a dependency.
---
--- Use the `dependencies` key to specify the dependencies of a particular plugin
---
--- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
--- Or use telescope!
--- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
--- you can continue same window with `<space>sr` which resumes last telescope search
+-- Configure and install plugins
+-- https://lazy.folke.io/installation
 require('lazy').setup({
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.debug',
@@ -71,17 +20,13 @@ require('lazy').setup({
   require 'plugins.vim-sleuth',
   require 'plugins.which-key',
 
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
   -- { import = 'custom.plugins' },
 }, {
   rocks = {
     enabled = false,
   },
   ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    -- Fallback to unicode icons if a Nerd Font is not available
     icons = vim.g.have_nerd_font and {} or {
       cmd = '⌘',
       config = '🛠',
