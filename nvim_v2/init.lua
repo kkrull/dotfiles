bd74c78681802e82f4fd31ac8d565d1bf2f9b4ea
Kyle Krull <kdkrull@gmail.com>
2025-12-06 11:42:15 -0600

    Extract options

--- nvim_v2/init.lua
+++ nvim_v2/init.lua
@@ -1,33 +1,4 @@
--- clipboard
-vim.opt.clipboard = "unnamedplus"
-
--- colors
-vim.opt.termguicolors = true
-
--- command preview
-vim.opt.inccommand = "split"
-
--- line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
-
--- search
-vim.opt.ignorecase = true
-
--- splits
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-
--- tabs
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-
--- visual mode
-vim.opt.virtualedit = "block"
-
--- wrapping
-vim.opt.wrap = true
+require("options")
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
---
+++ nvim_v2/options.lua
@@ -0,0 +1,31 @@
+-- clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- colors
+vim.opt.termguicolors = true
+
+-- command preview
+vim.opt.inccommand = "split"
+
+-- line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- search
+vim.opt.ignorecase = true
+
+-- splits
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- tabs
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+
+-- visual mode
+vim.opt.virtualedit = "block"
+
+-- wrapping
+vim.opt.wrap = true
+
