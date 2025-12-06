285c243ebab95ea7ac4d662994a2d295d33e0782
Kyle Krull <kdkrull@gmail.com>
2025-12-06 11:36:57 -0600

    Add package manager and color scheme

--- nvim_v2/README.md
+++ nvim_v2/README.md
@@ -11,3 +11,12 @@ make install
 ```shell
 make install-tools
 ```
+
+## Reference
+
+- [Understanding Neovim](https://youtu.be/87AXw9Quy9U?si=8UXxUVxhzah1XyIT)
+
+## Sources
+
+- [folke/lazy.nvim](https://lazy.folke.io/installation)
+- [folke/tokyonight.nvim](https://github.com/folke/tokyonight.nvim?tab=readme-ov-file#-installation)
--- nvim_v2/init.lua
+++ nvim_v2/init.lua
@@ -1,9 +1,18 @@
 -- clipboard
 vim.opt.clipboard = "unnamedplus"
 
+-- colors
+vim.opt.termguicolors = true
+
+-- command preview
+vim.opt.inccommand = "split"
+
 -- line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- search
+vim.opt.ignorecase = true
 
 -- splits
 vim.opt.splitbelow = true
@@ -11,7 +20,49 @@ vim.opt.splitright = true
 
 -- tabs
 vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+
+-- visual mode
+vim.opt.virtualedit = "block"
 
 -- wrapping
 vim.opt.wrap = true
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Set configuration before plugins load (and remember) it
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+-- Load lazy.nvim and any immediately-loaded plugins (like colorschemes)
+require("lazy").setup({
+  spec = {
+    {
+      "folke/tokyonight.nvim",
+      lazy = false,
+      priority = 1000,
+      opts = {},
+      config = function()
+        vim.cmd([[colorscheme tokyonight-moon]])
+      end,
+    }
+  },
+  install = { colorscheme = { "folke/tokyonight.nvim" } },
+  checker = { enabled = false },
+})
