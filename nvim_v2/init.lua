a17784d0a24f401d79fce8c68d75a492b7b498b2
Kyle Krull <kdkrull@gmail.com>
2025-12-06 11:06:09 -0600

    Add some basic configuration

---
+++ nvim_v2/init.lua
@@ -0,0 +1,17 @@
+-- clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- splits
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- tabs
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+
+-- wrapping
+vim.opt.wrap = true
