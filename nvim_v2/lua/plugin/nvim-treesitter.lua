f42ff74a875c0722188738fe73a619ca68dce3b0
Kyle Krull <kdkrull@gmail.com>
2025-12-18 16:32:04 -0600

    Upgrade nvim-treesitter

--- nvim_v2/lua/plugin/nvim-treesitter.lua
+++ nvim_v2/lua/plugin/nvim-treesitter.lua
@@ -4,6 +4,7 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    branch = 'main',
 
     -- lazy.nvim doesn't want you to write `config`: https://lazy.folke.io/spec#spec-setup
     --
@@ -14,26 +15,33 @@ return {
     -- Neatly summarized here:
     -- https://github.com/nvim-treesitter/nvim-treesitter/discussions/7535#discussioncomment-12842161
     config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
+      require('nvim-treesitter').install({
+        'c',
+        'lua',
+        'vim',
+        'vimdoc',
+      })
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { 'lua' },
+        callback = function()
+          -- syntax highlighting, provided by Neovim
+          vim.treesitter.start()
+          -- folds, provided by Neovim
+          vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          vim.wo.foldmethod = 'expr'
+          -- indentation, provided by nvim-treesitter
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+  })
+
     end,
 
     -- Does not support lazy-loading
     -- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#installation
     lazy = false,
-    opts = {
-      auto_install = true,
-      ensure_installed = {
-        'c',
-        'lua',
-        'vim',
-        'vimdoc'
-      },
-      highlight = {
-        enable = true
-      },
-      indent = {
-        enable = true
-      },
-    },
+
+    -- nvim-treesitter doesn't seem to have many opts that can go its its setup method, since v0.10 
+    opts = { },
   }
 }
