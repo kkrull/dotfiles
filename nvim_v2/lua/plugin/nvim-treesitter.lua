051d1cc825e21696abb3dc674af7bb38ae3b0294
Kyle Krull <kdkrull@gmail.com>
2025-12-18 15:29:02 -0600

    Use opts over config

--- nvim_v2/lua/plugin/nvim-treesitter.lua
+++ nvim_v2/lua/plugin/nvim-treesitter.lua
@@ -3,15 +3,16 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter.config').setup({
-      ensure_installed = { 'c', 'lua', 'vim', 'vimdoc' },
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+--  config = function(_plugin, opts)
+--    require('nvim-treesitter.configs').setup(opts)
+--  end,
 
   -- Does not support lazy-loading
   -- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#installation
   lazy = false,
+  opts = {
+    ensure_installed = { 'c', 'lua', 'vim', 'vimdoc' },
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
 }
