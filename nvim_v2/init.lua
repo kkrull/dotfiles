109bc5dd956828b4333bbcfc80e7e3cff737fafe
Kyle Krull <kdkrull@gmail.com>
2025-12-20 12:37:54 -0600

    Port to chezmoi

--- nvim_v2/init.lua
+++
@@ -1,6 +0,0 @@
-require('01-keymaps')
-require('01-options')
-require('02a-lazy-bootstrap')
-require('02b-lazy-init')
-
--- vim: ts=2 sts=2 sw=2 et
--- nvim_v2/lua/plugin/README.md
+++
@@ -1,5 +0,0 @@
-# Lazy.nvim plugins
-
-Plugins that are loaded by `lazy.nvim`.
-
-See <https://lazy.folke.io/spec#spec-setup> for details.
